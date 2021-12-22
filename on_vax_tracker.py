b34c268381c899120fb3ae27cb24a1b1bf718fa1
Dayi Lin <heylindayi@gmail.com>
2021-12-22 06:55:21 -0500

    Update on_vax_tracker.py

--- on_vax_tracker.py
+++ on_vax_tracker.py
@@ -7,7 +7,7 @@ import os
 
 VAX_URL_PREFIX = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=775ca815-5028-4e9b-9dd4-6975ff1be021&sort=_id%20asc&filters={%22Date%22:%22"
 VAX_URL_POSTFIX = "%22}"
-ON_POPULATION = 14755211    #Source: https://www.fin.gov.on.ca/en/economy/demographics/quarterly/
+ON_POPULATION = 14826276    #Source: https://www.fin.gov.on.ca/en/economy/demographics/quarterly/
 
 BAR_LENGTH = 10
 PROGRESS_BAR = ['⬛️', '🔳', '⬜️']
@@ -23,15 +23,14 @@ TWEET_TEMPLATE = '''On {report_date}, #Ontario reported:
 
 % population vaccinated with at least 1 dose:
 {first_dose_bar} {first_dose_percentage}%
-12+ {first_dose_eligible}%
-18+ {first_dose_adult}%
+5-11 years old: {first_dose_5to11}%
+Eligible: {first_dose_eligible}%
 
 % population fully vaccinated:
 {full_dose_bar} {full_dose_percentage}%
-12+ {full_dose_eligible}%
-18+ {full_dose_adult}%
+5-11 years old: {full_dose_5to11}%
+Eligible: {full_dose_eligible}%
 
-Total doses administered: {total_dose}
 #OntarioVaccine @VaxHuntersON'''
 
 twitter_auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
@@ -53,32 +52,32 @@ def update_vac_progress(last_date, debug = True):
         vax_data = {}
         for record in vax_response_obj["result"]["records"]:
             vax_data[record["Agegroup"]] = record
-        population_12to17   = int(vax_data["12-17yrs"]["Total population"])
-        population_adult    = int(vax_data["Adults_18plus"]["Total population"])
-        atleast1dose_12to17 = int(vax_data["12-17yrs"]["At least one dose_cumulative"])
-        atleast1dose_adult  = int(vax_data["Adults_18plus"]["At least one dose_cumulative"])
-        two_dose_12to17     = int(vax_data["12-17yrs"]["Second_dose_cumulative"])
-        two_dose_adult      = int(vax_data["Adults_18plus"]["Second_dose_cumulative"])
+        population_5to11       = int(vax_data["05-11yrs"]["Total population"])
+        population_eligible    = int(vax_data["Ontario_5plus"]["Total population"])
+        atleast1dose_5to11     = int(vax_data["05-11yrs"]["At least one dose_cumulative"])
+        atleast1dose_eligible  = int(vax_data["Ontario_5plus"]["At least one dose_cumulative"])
+        two_dose_5to11         = int(vax_data["05-11yrs"]["fully_vaccinated_cumulative"])
+        two_dose_eligible      = int(vax_data["Ontario_5plus"]["fully_vaccinated_cumulative"])
 
-        total_dose = atleast1dose_12to17 + two_dose_12to17 + atleast1dose_adult + two_dose_adult + int(vax_data["Undisclosed_or_missing"]["At least one dose_cumulative"]) + int(vax_data["Undisclosed_or_missing"]["Second_dose_cumulative"])
+        total_dose = atleast1dose_eligible + two_dose_eligible + int(vax_data["Undisclosed_or_missing"]["At least one dose_cumulative"]) + int(vax_data["Undisclosed_or_missing"]["fully_vaccinated_cumulative"])
 
-        atleast1dose_adult_percent = round(atleast1dose_adult * 100.0 / population_adult, 2)
-        two_dose_adult_percent = round(two_dose_adult * 100.0 / population_adult, 2)
-        atleast1dose_eligible_percent = round((atleast1dose_adult+atleast1dose_12to17) * 100.0 / (population_adult+population_12to17), 2)
-        two_dose_eligible_percent = round((two_dose_adult+two_dose_12to17) * 100.0 / (population_adult+population_12to17), 2)
-        atleast1dose_population_percent = round((atleast1dose_adult+atleast1dose_12to17) * 100.0 / ON_POPULATION, 2)
-        two_dose_population_percent = round((two_dose_adult+two_dose_12to17) * 100.0 / ON_POPULATION, 2)
+        atleast1dose_5to11_percent = round(atleast1dose_5to11 * 100.0 / population_5to11, 2)
+        two_dose_5to11_percent = round(two_dose_5to11 * 100.0 / population_5to11, 2)
+        atleast1dose_eligible_percent = round(atleast1dose_eligible * 100.0 / population_eligible, 2)
+        two_dose_eligible_percent = round(two_dose_eligible * 100.0 / population_eligible, 2)
+        atleast1dose_population_percent = round(atleast1dose_eligible * 100.0 / ON_POPULATION, 2)
+        two_dose_population_percent = round(two_dose_eligible * 100.0 / ON_POPULATION, 2)
 
         first_dose_bar = bar_str(atleast1dose_population_percent)
         full_dose_bar = bar_str(two_dose_population_percent)
 
         tweet_text = TWEET_TEMPLATE.format( first_dose_bar = first_dose_bar, 
                                             first_dose_percentage = atleast1dose_population_percent,
-                                            first_dose_adult = atleast1dose_adult_percent,
+                                            first_dose_5to11 = atleast1dose_5to11_percent,
                                             first_dose_eligible = atleast1dose_eligible_percent,
                                             full_dose_bar = full_dose_bar,
                                             full_dose_percentage = two_dose_population_percent,
-                                            full_dose_adult = two_dose_adult_percent,
+                                            full_dose_5to11 = two_dose_5to11_percent,
                                             full_dose_eligible = two_dose_eligible_percent,
                                             report_date = check_date.strftime("%Y-%m-%d"),
                                             total_dose = f'{total_dose:,}' ) #,
@@ -101,7 +100,7 @@ def update_vac_progress(last_date, debug = True):
 def main():
     try:
         with open(STATUS_PATH, 'r') as status_file:
-            last_date = datetime.strptime(status_file.read(), "%Y-%m-%d")
+            last_date = datetime.strptime(status_file.read().strip(), "%Y-%m-%d")
         update_vac_progress(last_date, debug=False)
     except Exception as ex:
         print("[" + str(datetime.now()) + "] [ERROR] Unknown error:\n" + str(ex))
